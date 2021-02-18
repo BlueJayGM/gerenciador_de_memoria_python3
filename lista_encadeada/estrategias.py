52937d435ca57f70d3c371050de70d7f48d1833b
Mikhael D'Amato <mikhael@devtag.com.br>
2021-02-17 22:19:44 -0300

    resolvendo bug do first-fit

--- lista_encadeada/estrategias.py
+++ lista_encadeada/estrategias.py
@@ -14,26 +14,26 @@ class Estrategia:
           processo.id = 0
           self.identificadores[0] = processo.tamanho # adiciona o tamanho e o id 
           return True
-      else:
-        currentProcess = self.processo_main # pega a cabeca como atual
-        while currentProcess.processo != None: # pega o ultimo processo
-          atual = currentProcess.processo
-          prox = atual.processo
-          if prox != None:
-            if atual.id + atual.tamanho == prox.id:
-              currentProcess = currentProcess.processo
-            else:
-              if atual.id + atual.tamanho + processo.tamanho < prox.id:
-                currentProcess = currentProcess.processo
-                break
-          else:
+    else:
+      currentProcess = self.processo_main # pega a cabeca como atual
+      while currentProcess.processo != None: # pega o ultimo processo
+        atual = currentProcess.processo
+        prox = atual.processo
+        if prox != None:
+          if atual.id + atual.tamanho == prox.id:
             currentProcess = currentProcess.processo
-        id = currentProcess.id + currentProcess.tamanho # gera o id do novo processo
-        if processo.tamanho + id - 1 <= self.tamanho_memoria: # guarda o processo
-          processo.id = id
-          self.identificadores[id] = processo.tamanho
-          currentProcess.processo = processo
-          return True
+          else:
+            if atual.id + atual.tamanho + processo.tamanho < prox.id:
+              currentProcess = currentProcess.processo
+              break
+        else:
+          currentProcess = currentProcess.processo
+      id = currentProcess.id + currentProcess.tamanho # gera o id do novo processo
+      if processo.tamanho + id - 1 <= self.tamanho_memoria: # guarda o processo
+        processo.id = id
+        self.identificadores[id] = processo.tamanho
+        currentProcess.processo = processo
+        return True
       return False
     
   def bestFit(self, processo):
